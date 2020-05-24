8c7e3c62c162cfa70a73d4e43a660816a5871015
Priscila Cabral <priscilacabral.dev@gmail.com>
2020-05-24 14:01:54 -0300

    Inserting labels and input into the app

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "IMC Application",
-      theme: ThemeData(
-        primarySwatch: Colors.purple
-      ),
+      theme: ThemeData(primarySwatch: Colors.purple),
       home: Home(),
     );
   }
@@ -27,9 +25,44 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.purple,
-        title: Text("IMC"),
-        centerTitle: true
+          backgroundColor: Colors.teal, title: Text("IMC"), centerTitle: true),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Image.network(
+                  "https://pt.calcuworld.com/wp-content/uploads/sites/6/2019/07/imc-.png"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: TextField(
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(15),
+                    labelText: "Nome",
+                    border: OutlineInputBorder()),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: TextField(
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(15),
+                    labelText: "Peso",
+                    border: OutlineInputBorder()),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: TextField(
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(15),
+                    labelText: "Altura",
+                    border: OutlineInputBorder()),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
